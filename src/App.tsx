3259ab06bee013e39c8ca5fba4aec4ac73ab9878
nakashi94 <n0904yBUNJOU@gmail.com>
2021-12-13 23:13:39 +0900

    create pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+// 外部モジュールからのインポート
 import { Button, ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+
+// ローカルファイルからのインポート
 import theme from './theme/theme'
 
 function App() {
   return (
     <div>
       <ChakraProvider theme={theme}>
-        <Button colorScheme="teal">ボタン</Button>
+        <BrowserRouter>
+          <Button colorScheme="teal">ボタン</Button>
+        </BrowserRouter>
       </ChakraProvider>
     </div>
   );
---
+++ src/router/Router.tsx
@@ -0,0 +1,7 @@
+import { memo, VFC } from 'react';
+
+export const Router = memo(() => {
+    return (
+        <p>a</p>
+    )
+})
