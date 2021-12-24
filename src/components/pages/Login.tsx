9eaef36b5c8c1bf7ed43247464456dccaddaf8a6
nakashi94 <n0904yBUNJOU@gmail.com>
2021-12-25 01:24:01 +0900

    page404

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -4,10 +4,10 @@ import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login: VFC = memo(() => {
     return (
-        <Flex align="center" justify="center" height="100v">
-            <Box bg="white" w="sm" p={4} borderRadius="">
+        <Flex align="center" justify="center" height="100vh">
+            <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
                 <Heading as="h1" size="lg" textAlign="center">ユーザー管理アプリ</Heading>
-                <Divider />
+                <Divider my={4} />
                 <Stack spacing={6} py={4} px={10}>
                     <Input placeholder="ユーザーID" />
                     <Button bg="teal.400" color="white">ログイン</Button>
@@ -15,4 +15,4 @@ export const Login: VFC = memo(() => {
             </Box>
         </Flex>
     )
-})
+});
--- src/components/pages/Page404.tsx
+++ src/components/pages/Page404.tsx
@@ -2,6 +2,9 @@ import { memo, FC } from 'react';
 
 export const Page404: FC = memo(() => {
     return (
-        <p>404ページ</p>
+        <>
+            <p>404ページ</p>
+            <p>お探しのページは存在しません。</p>
+        </>
     )
-})
+});
