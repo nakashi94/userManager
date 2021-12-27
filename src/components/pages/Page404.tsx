3a965133f109b548475c33eb81d274651be220aa
nakashi94 <n0904yBUNJOU@gmail.com>
2021-12-27 23:43:05 +0900

    404

--- src/components/pages/Page404.tsx
+++ src/components/pages/Page404.tsx
@@ -4,7 +4,7 @@ export const Page404: FC = memo(() => {
     return (
         <>
             <p>404ページ</p>
-            <p>お探しのページは存在しません。</p>
+            <p>This page is not fount.</p>
         </>
     )
 });
