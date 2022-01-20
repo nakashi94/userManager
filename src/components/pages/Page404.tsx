aebeb8815e20bc0c0f5fb00376575b0e71b405a8
nakashi94 <n0904yBUNJOU@gmail.com>
2022-01-20 23:58:38 +0900

    link to homepage

--- src/components/pages/Page404.tsx
+++ src/components/pages/Page404.tsx
@@ -1,10 +1,12 @@
 import { memo, FC } from 'react';
+import { Link } from "react-router-dom";
 
 export const Page404: FC = memo(() => {
     return (
         <>
             <p>404ページ</p>
             <p>This page is not fount.</p>
+            <Link to="/home">homepageはこちら</Link>
         </>
     )
 });
