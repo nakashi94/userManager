a27b9f812236b5f55180559aa37ca91e6bbb81fb
nakashi94 <n0904yBUNJOU@gmail.com>
2022-01-21 00:24:33 +0900

    @types/styled-components

--- src/components/pages/Page404.tsx
+++ src/components/pages/Page404.tsx
@@ -1,12 +1,19 @@
 import { memo, FC } from 'react';
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export const Page404: FC = memo(() => {
     return (
         <>
             <p>404ページ</p>
             <p>This page is not fount.</p>
-            <Link to="/home">homepageはこちら</Link>
+                <Link to="/home">
+                    <SLinkWrapper>homepageはこちら</SLinkWrapper>
+                </Link>
         </>
     )
 });
+
+const SLinkWrapper = styled.div`
+    color: #1111ff;
+`
